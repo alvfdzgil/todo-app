a5bbecec3173c9f1d96cfdf055d7103b7878a254
alvfdzgil <afernandez@afiglobaleducation.es>
2025-12-14 20:41:29 +0100

    otro

--- code/app.py
+++ code/app.py
@@ -1,10 +1,3 @@
-import sys
-from pathlib import Path
-
-ROOT = Path(__file__).resolve().parent.parent
-if str(ROOT) not in sys.path:
-    sys.path.insert(0, str(ROOT))
-
 import streamlit as st
 from PIL import Image 
 
